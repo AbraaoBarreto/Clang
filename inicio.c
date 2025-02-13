b31b5911568d561831f19124fc1d5d01b4198528
AbraaoBarreto <abraaobarreto150@gmail.com>
2025-02-13 23:00:53 +0000

    primeiro estagio

--- inicio.c
+++ inicio.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
+
+//PRIMEIRA PARTE DE DESAFIO, CADASTRO DE CIDADES
+
 int main() {
     int pontos_turisticos; 
-    float populacao, PIB, area;
+    float populacao, pib, area;
     char codigo[4];
     char cidade[20];
 
-    printf("Insira o nome da cidade: \n");
-    scanf("%s", &cidade);
+    printf("Insira o nome da cidade(use _ ao inves de " "): \n");
+    scanf(" %s", cidade);
 
     printf("insira o codigo da cidade: \n");
-    scanf("%s", &codigo);
+    scanf(" %s", codigo);
 
-    printf(" insira a população da cidade (Em milhoes): \n");
+    printf("insira a população da cidade (Em milhoes): \n");
     scanf("%f", &populacao);
 
     printf("Insira o PIB da cidade (Em bilhoes): \n");
-    scanf("%f", &PIB);
+    scanf("%f", &pib);
 
-    printf("Insira o numerod e pontos turisticos da cidade: \n");
+    printf("Insira o numero de pontos turisticos da cidade: \n");
     scanf("%d", &pontos_turisticos);
 
-    printf("insira a area da cidade (em km): \n");
+    printf("insira a area da cidade (em km²): \n");
     scanf("%f", &area);
 
 
-    printf("***********  CIDADE   ************");
-    printf("Cidade: %s.\n Codigo: %s.\n -População: %f.\n -PIB: %f.\n -Pontos turisticos: %d.\n -Área: %f.\n ", cidade, codigo, populacao, PIB, pontos_turisticos, area);
+    printf("***********  CIDADE   ************\n");
+    printf("Cidade: %s.\n Codigo: %s.\n -População: %.2f.\n -PIB: %.2f.\n -Pontos turisticos: %d.\n -Área: %.2f.\n ", cidade, codigo, populacao, pib, pontos_turisticos, area);
 
     return 0;
 }
